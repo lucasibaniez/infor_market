cd30e46bc4921e1fee1c4b4fe447c368ca35c8a2
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-11-29 19:32:11 -0300

    conectamos a bd

--- Market/Market/views.py
+++ Market/Market/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+from apps.productos.models import Producto
+
 def inicio(request):
-	return render(request, "inicio.html")
+	productos = Producto.objects.all()
+	p1 = Producto.objects.get(id=1)
+	print("===================")
+	print(p1)
+	# print(p1.query)
+	print("===================")
+
+	usuario = {
+		"nombre": "Lucas",
+		"apellido": "Ibañez"
+	}
+
+	context = {
+		"usuario": usuario,
+		"productos": productos
+	}
+	return render(request, "inicio.html", context)
