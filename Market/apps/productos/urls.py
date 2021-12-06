6792801f0f16132fca9797cf4f5ef660156558ab
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-12-06 17:42:19 -0300

    template base

---
+++ Market/apps/productos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+
+from . import views
+
+app_name = "productos"
+
+urlpatterns = [
+	path("Detalle/", views.detalle, name="detalle")
+]
--- Market/apps/productos/views.py
+++ Market/apps/productos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def detalle(request):
+	context = {}
+	return render(request, "productos/detalle.html", context)
