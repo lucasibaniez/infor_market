fe4f7aa5fb4c1709f91f312c424914d0cae7aa35
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-12-06 18:17:01 -0300

    list view productos

--- Market/apps/productos/urls.py
+++ Market/apps/productos/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "productos"
 
 urlpatterns = [
-	path("Detalle/", views.detalle, name="detalle")
+	path("Detalle/", views.detalle, name="detalle"),
+	path("Admin/Listar/", views.ListarAdmin.as_view(), name="admin_listar")
+
+
 ]
--- Market/apps/productos/views.py
+++ Market/apps/productos/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts     import render
+from django.views.generic import ListView
+
+from .models import Producto
 
 def detalle(request):
 	context = {}
 	return render(request, "productos/detalle.html", context)
+
+
+class ListarAdmin(ListView):
+	template_name="productos/admin/listar.html"
+	model = Producto
+	context_object_name="productos"
