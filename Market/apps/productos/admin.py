06b5c3448ee7d3e0cac5372a77e2b297c28e67c6
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-12-07 19:09:18 -0300

    relaciones entre tablas

--- Market/apps/productos/admin.py
+++ Market/apps/productos/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Producto
+from .models import Producto, Categoria
+
+
+admin.site.register(Categoria)
 
 admin.site.register(Producto)
+
 
--- Market/apps/productos/models.py
+++ Market/apps/productos/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	class Meta:
+		db_table="categorias"
+	def __str__(self):
+		return self.nombre
+
+# OneToOne (Investigar)
+
 class Producto(models.Model):
 	nombre = models.CharField(max_length=255)
 	precio = models.DecimalField(max_digits=9, decimal_places=2)
+
+	# categorias = models.ManyToManyField(Categoria)
+
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+
+	
 
 	class Meta:
 		db_table="productos"
