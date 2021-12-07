11d65fb29efe22ea9841074e56be357a2d379366
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-12-07 17:43:00 -0300

    controles en formularios

--- Market/apps/usuarios/forms.py
+++ Market/apps/usuarios/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
 from . models import Usuario
 
 class UsuarioForm(UserCreationForm):
@@ -7,9 +9,16 @@ class UsuarioForm(UserCreationForm):
 	first_name = forms.CharField(label="Nombre", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":"Ingrese nombre"}))
 	last_name = forms.CharField(label="Apellido", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":"Ingrese su apellido"}))
 	
-	password = forms.CharField(widget=forms.PasswordInput())
-	password1 = forms.CharField(widget=forms.PasswordInput())
+	#password = forms.CharField(widget=forms.PasswordInput())
+	#password2 = forms.CharField(widget=forms.PasswordInput())
 
 	class Meta:
 		model = Usuario
 		fields = ["username", "first_name", "last_name", "email", "dni"]
+
+	def clean_username(self):
+		username = self.cleaned_data["username"]
+
+		if "123" not in username:
+			raise ValidationError("El nombre de usuario debe incluir la cadena 123") 
+		return username
